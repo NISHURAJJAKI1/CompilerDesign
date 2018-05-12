6cff288202eb588048e47028b5042391a88fe405
NISHURAJJAKI1 <39029251+NISHURAJJAKI1@users.noreply.github.com>
2018-05-12 07:00:55 +0000

    Delete _2d.java

--- _2d.java
+++
@@ -1,9 +0,0 @@
-
-public class _2d {
-	public static void main(String[] args){
-		String s="Wasiul Mannan";  
-		   System.out.println(s.substring(0,6));  
-		   System.out.println(s.substring(7));
-	}
-
-}
